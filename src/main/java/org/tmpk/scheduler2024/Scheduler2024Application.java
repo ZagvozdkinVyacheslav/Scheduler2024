5b0e6d746f1377fab62bc618047d48fcabf58b32
v.zagvozdkin <v.zagvozdkin@cloudcom.ru>
2024-03-31 15:09:58 +0300

    Initial commit

---
+++ src/main/java/org/tmpk/scheduler2024/Scheduler2024Application.java
@@ -0,0 +1,13 @@
+package org.tmpk.scheduler2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scheduler2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scheduler2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/tmpk/scheduler2024/Scheduler2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tmpk.scheduler2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scheduler2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
